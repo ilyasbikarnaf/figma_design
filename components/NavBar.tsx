f3f3c23e08b50aa0d0ce5c222265aa27e2192ed8
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-05-20 18:49:19 +0100

    add Navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,13 +3,24 @@ import Heart from "@/assets/heart.svg";
 import Bell from "@/assets/bell.svg";
 import Profile from "@/assets/profile.svg";
 import Settings from "@/assets/settings.svg";
+import SearchIcon from "@/assets/searchIcon.svg";
+import FilterIcon from "@/assets/filterIcon.svg";
 
 export default function NavBar() {
   return (
-    <header className="flex justify-between bg-white py-6 px-6 items-center rounded">
+    <header className="flex justify-between bg-white py-6 px-20 items-center rounded">
       <div className="flex items-center gap-20">
-        <h2 className="text-[#3563E9] text-4xl">LOGO</h2>
-        <div>search bar</div>
+        <h2 className="text-primary text-4xl">LOGO</h2>
+        <div className="flex py-3 px-5 gap-4 items-center border-2 rounded-full border-[#C3D4E966]">
+          <Image alt="search icon" src={SearchIcon} />
+          <input
+            type="text"
+            placeholder="Search Something Here"
+            className="w-80 focus:outline-none text-secondary"
+          />
+
+          <Image alt="filter icon" src={FilterIcon} />
+        </div>
       </div>
       <nav className="flex gap-4 items-center">
         <button className=" border-[#C3D4E966] border-1 size-7 rounded-2xl flex justify-center">
